06f7f3fc631371828e7c4ddcbbc04626ee840f92
ramsir3 <ramsir3@gmail.com>
2022-01-10 15:52:54 -0800

    cnn+rnn

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import pandas as pd
 import numpy as np
 
 # %%
-from model import EncoderRNN, AttnDecoder, train, timeSince, tensorFromArr, device
+from model import Encoder, AttnDecoder, train, timeSince, tensorFromArr, device
 
 # %%
 import time
@@ -39,13 +39,14 @@ data_y = le.transform(data_y['label'])
 # %%
 data_x[:, 1:] = MinMaxScaler().fit_transform(data_x[:, 1:])
 data_x = np.expand_dims(data_x, 1)
-data_x = np.moveaxis(data_x, 2, 0)
+# data_x = np.moveaxis(data_x, 2, 0)
 
 print(data_x.shape, data_y.shape)
 
 # %%
-def trainIters(encoder, decoder, n_iters, print_every=1000, plot_every=100, learning_rate=0.01):
+def trainIters(encoder, decoder, n_iters, print_every=100, plot_every=100, learning_rate=0.01):
     start = time.time()
+    print(start)
     plot_losses = []
     print_loss_total = 0  # Reset every print_every
     plot_loss_total = 0  # Reset every plot_every
@@ -54,7 +55,7 @@ def trainIters(encoder, decoder, n_iters, print_every=1000, plot_every=100, lear
     decoder_optimizer = optim.SGD(decoder.parameters(), lr=learning_rate)
     criterion = nn.CrossEntropyLoss()
 
-    for iter in range(1, n_iters + 1):
+    for iter in range(1, n_iters+1):
         input_tensor, target_tensor = tensorFromArr(data_x, data_y, random.choice(range(data_x.shape[1])))
 
         loss = train(input_tensor, target_tensor, encoder,
@@ -86,11 +87,11 @@ def showPlot(points):
 
 # %%
 nhidden = 100
-enc = EncoderRNN(1, nhidden).to(device)
+enc = Encoder(1, 1, nhidden).to(device)
 dec = AttnDecoder(nhidden, data_y.max()+1).to(device)
 
 # %%
-pl = trainIters(enc, dec, int(1e0))
+pl = trainIters(enc, dec, int(1e5))
 
 # %%
 showPlot(pl)
--- model.py
+++ model.py
@@ -11,33 +11,47 @@ import math
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-MAX_LENGTH = 396900
+MAX_LENGTH = 770
 
-class EncoderRNN(nn.Module):
-    def __init__(self, input_size, hidden_size):
-        super(EncoderRNN, self).__init__()
+class Encoder(nn.Module):
+    def __init__(self, input_size, conv_output, hidden_size, dropout_p=0.1):
+        super(Encoder, self).__init__()
         self.hidden_size = hidden_size
+        self.conv1_output = conv_output
+        self.dropout_p = dropout_p
 
-        self.gru = nn.GRU(input_size=input_size, hidden_size=hidden_size)
+        self.conv1 = nn.Conv1d(
+            in_channels=input_size,
+            out_channels=conv_output,
+            kernel_size=3072,
+            stride=512,
+            padding=0,
+            )
+        self.dropout = nn.Dropout(self.dropout_p)
+        self.gru = nn.GRU(input_size=self.conv1_output, hidden_size=hidden_size)
 
     def forward(self, input, hidden):
-        output, hidden = self.gru(input.view(1, 1, -1), hidden)
-        return output, hidden
+        output = self.conv1(input)
+        output = self.dropout(output)
+        output = torch.movedim(output, 2, 0)
+        outputs = []
+        for ei in output:
+            output_i, hidden = self.gru(ei.unsqueeze(0), hidden)
+            outputs.append(output_i)
+        return torch.cat(outputs, dim=1), hidden
 
     def initHidden(self):
         return torch.zeros(1, 1, self.hidden_size, device=device)
 
 class AttnDecoder(nn.Module):
-    def __init__(self, hidden_size, output_size, dropout_p=0.1, max_length=MAX_LENGTH):
+    def __init__(self, hidden_size, output_size, max_length=MAX_LENGTH):
         super(AttnDecoder, self).__init__()
         self.hidden_size = hidden_size
         self.output_size = output_size
-        self.dropout_p = dropout_p
         self.max_length = max_length
 
         self.attn = nn.Linear(self.hidden_size, self.max_length)
         self.attn_combine = nn.Linear(self.hidden_size, self.hidden_size)
-        self.dropout = nn.Dropout(self.dropout_p)
         self.out = nn.Linear(self.hidden_size, self.output_size)
 
     def forward(self, hidden, encoder_outputs):
@@ -45,12 +59,7 @@ class AttnDecoder(nn.Module):
         attn_weights = F.softmax(
             self.attn(hidden), dim=1)
 
-        try:
-            attn_applied = torch.bmm(attn_weights,
-                                 encoder_outputs.unsqueeze(0))
-        except RuntimeError as e:
-            print(4, attn_weights.shape, encoder_outputs.shape)
-            raise e
+        attn_applied = torch.bmm(attn_weights, encoder_outputs)
 
         output = attn_applied
         output = self.attn_combine(output)
@@ -84,19 +93,19 @@ def train(input_tensor, target, encoder, decoder, encoder_optimizer, decoder_opt
 
     encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
 
-    for ei in range(input_length):
-        encoder_output, encoder_hidden = encoder(
-            input_tensor[ei], encoder_hidden)
-        encoder_outputs[ei] = encoder_output[0, 0]
-    # encoder_outputs, encoder_hidden = encoder(
-    #     input_tensor, encoder_hidden)
+    # for ei in range(input_length):
+    #     print('ei:', ei)
+    #     encoder_output, encoder_hidden = encoder(
+    #         input_tensor[ei], encoder_hidden)
+    #     encoder_outputs[ei] = encoder_output[0, 0]
+    encoder_outputs, encoder_hidden = encoder(
+        input_tensor, encoder_hidden)
 
 
     decoder_hidden = encoder_hidden
 
     decoder_output, decoder_hidden, decoder_attention = decoder(
         decoder_hidden, encoder_outputs)
-
     loss = criterion(decoder_output.squeeze(0), target)
 
     loss.backward()
@@ -109,7 +118,7 @@ def train(input_tensor, target, encoder, decoder, encoder_optimizer, decoder_opt
 def tensorFromArr(X, Y, i):
     nframes = int(X[0, i, 0])
     # print(i, nframes)
-    xi = X[1:nframes+1, i, :]
+    xi = X[i, :, 1:nframes+1]
     xi = np.expand_dims(xi, 1)
     yi = Y[i]
     xo = torch.tensor(xi,
